eca9f92ffecfb1acefe9ad4df04b74e50c527ca8
Soboliev Evgeniy <Soboliev Evgeniy>
2017-03-25 00:50:07 +0200

    Initial commit

---
+++ src/main/java/com/jackson/SpringBootCiApplication.java
@@ -0,0 +1,23 @@
+package com.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCiApplication.class, args);
+    }
+
+    @RequestMapping(value = "/{name}")
+    @ResponseBody
+    public String basePage(@PathVariable("name") String name) {
+        return "You are is: " + name;
+    }
+}
